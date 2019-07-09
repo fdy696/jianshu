5a7fdda0c7e23627472f8b4f21bb5fa43b8dac80
fdy696 <3456283371@qq.com>
2019-07-09 08:47:35 +0800

    Updates

---
+++ precache-manifest.70b530da2e06b373551b80e45d47fd47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57b7bb4fac361e40b958d4ae43da677",
+    "url": "/jianshupublish/index.html"
+  },
+  {
+    "revision": "69edd3acc2f9c60d92b4",
+    "url": "/jianshupublish/static/css/2.74bdaf1d.chunk.css"
+  },
+  {
+    "revision": "adff48c37db0cfed59f4",
+    "url": "/jianshupublish/static/css/main.023bd3a5.chunk.css"
+  },
+  {
+    "revision": "69edd3acc2f9c60d92b4",
+    "url": "/jianshupublish/static/js/2.de7c2dad.chunk.js"
+  },
+  {
+    "revision": "ac77749c2afcb4697d86",
+    "url": "/jianshupublish/static/js/3.d9beffeb.chunk.js"
+  },
+  {
+    "revision": "adff48c37db0cfed59f4",
+    "url": "/jianshupublish/static/js/main.fca6c5a1.chunk.js"
+  },
+  {
+    "revision": "1c81125dc121d7142ca0",
+    "url": "/jianshupublish/static/js/runtime~main.b844cfd7.js"
+  },
+  {
+    "revision": "eb3edc44e44f443feebf7c98215444da",
+    "url": "/jianshupublish/static/media/follow.eb3edc44.png"
+  }
+]);
